742b2060f353efc513b61a449c82b5aedd0a2210
mawencan <mawencan@yixinli.onaliyun.com>
2022-02-18 18:25:43 +0800

    chore: 改用压缩

--- scripts/build.js
+++ scripts/build.js
@@ -1,25 +1,43 @@
 import config from '../configs/rollup.config.settings.js'
 const _fn = function (moduleName) {
-  return {
-    'es': {
+  return [
+    {
+      entry: 'src/index.js',
+      // 当文件名包含 .min 时将会自动启用 terser 进行压缩
+      dest: `dist/${moduleName}.esm.min.js`,
+      format: 'es'
+    },
+    {
+      entry: 'src/index.js',
+      // 当文件名包含 .min 时将会自动启用 terser 进行压缩
+      dest: `dist/${moduleName}.cjs.min.js`,
+      format: 'cjs'
+    },
+    {
+      entry: 'src/index.js',
+      // 当文件名包含 .min 时将会自动启用 terser 进行压缩
+      dest: `dist/${moduleName}.min.js`,
+      format: 'iife'
+    },
+    {
       entry: 'src/index.js',
       // 当文件名包含 .min 时将会自动启用 terser 进行压缩
       dest: `dist/${moduleName}.esm.js`,
       format: 'es'
     },
-    'cjs': {
+    {
       entry: 'src/index.js',
       // 当文件名包含 .min 时将会自动启用 terser 进行压缩
       dest: `dist/${moduleName}.cjs.js`,
       format: 'cjs'
     },
-    'iife': {
+    {
       entry: 'src/index.js',
       // 当文件名包含 .min 时将会自动启用 terser 进行压缩
       dest: `dist/${moduleName}.js`,
       format: 'iife'
     }
-  }
+  ]
 }
 export default function build (fn = _fn) {
   config(fn)
